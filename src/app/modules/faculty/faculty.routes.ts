b9d0cec33367557b9b83ef0e3cb5d61ceed77936
arfat132 <arfatbegum90@gmail.com>
2023-09-05 21:49:23 +0300

    [Done] faculty module

---
+++ src/app/modules/faculty/faculty.routes.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { FacultyController } from './faculty.controller';
+import { FacultyValidation } from './faculty.validations';
+
+const router = express.Router();
+
+router.get('/', FacultyController.getAllFaculty);
+
+router.get('/:id', FacultyController.getFaculty);
+
+
+router.post(
+    '/',
+    validateRequest(FacultyValidation.create),
+    FacultyController.createFaculty
+);
+
+router.patch(
+    '/:id',
+    validateRequest(FacultyValidation.update),
+    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    FacultyController.updateFaculty
+);
+
+router.delete(
+    '/:id',
+    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    FacultyController.deleteFaculty
+);
+
+router.post(
+    '/:id/assign-courses',
+    validateRequest(FacultyValidation.assignOrRemoveCourses),
+    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    FacultyController.assignCourses)
+
+router.delete(
+    '/:id/remove-courses',
+    validateRequest(FacultyValidation.assignOrRemoveCourses),
+    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    FacultyController.removeCourses)
+
+export const facultyRoutes = router;
---
+++ src/app/modules/faculty/faculty.service.ts
@@ -0,0 +1,190 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { CourseFaculty, Faculty, Prisma } from "@prisma/client";
+import { paginationHelpers } from "../../../helpers/paginationHelper";
+import { IGenericResponse } from "../../../interfaces/common";
+import { IPaginationOptions } from "../../../interfaces/pagination";
+import prisma from "../../../shared/prisma";
+import { facultyRelationalFields, facultyRelationalFieldsMapper, facultySearchableFields } from "./faculty.constants";
+import { IFacultyFilterRequest } from "./faculty.interface";
+
+const createFaculty = async (data: Faculty): Promise<Faculty> => {
+    const result = await prisma.faculty.create({
+        data,
+        include: {
+            academicFaculty: true,
+            academicDepartment: true
+        }
+    });
+    return result;
+};
+
+const getAllFaculty = async (
+    filters: IFacultyFilterRequest,
+    options: IPaginationOptions
+): Promise<IGenericResponse<Faculty[]>> => {
+    const { limit, page, skip } = paginationHelpers.calculatePagination(options);
+    const { searchTerm, ...filterData } = filters;
+
+    const andConditions = [];
+
+    if (searchTerm) {
+        andConditions.push({
+            OR: facultySearchableFields.map((field:any) => ({
+                [field]: {
+                    contains: searchTerm,
+                    mode: 'insensitive'
+                }
+            }))
+        });
+    }
+
+    if (Object.keys(filterData).length > 0) {
+        andConditions.push({
+            AND: Object.keys(filterData).map((key) => {
+                if (facultyRelationalFields.includes(key)) {
+                    return {
+                        [facultyRelationalFieldsMapper[key]]: {
+                            id: (filterData as any)[key]
+                        }
+                    };
+                } else {
+                    return {
+                        [key]: {
+                            equals: (filterData as any)[key]
+                        }
+                    };
+                }
+            })
+        });
+    }
+
+    const whereConditions: Prisma.FacultyWhereInput =
+        andConditions.length > 0 ? { AND: andConditions } : {};
+
+    const result = await prisma.faculty.findMany({
+        include: {
+            academicFaculty: true,
+            academicDepartment: true
+        },
+        where: whereConditions,
+        skip,
+        take: limit,
+        orderBy:
+            options.sortBy && options.sortOrder
+                ? { [options.sortBy]: options.sortOrder }
+                : {
+                    createdAt: 'desc'
+                }
+    });
+    const total = await prisma.faculty.count({
+        where: whereConditions
+    });
+
+    return {
+        meta: {
+            total,
+            page,
+            limit
+        },
+        data: result
+    };
+};
+
+const getFaculty = async (id: string): Promise<Faculty | null> => {
+    const result = await prisma.faculty.findUnique({
+        where: {
+            id
+        },
+        include: {
+            academicFaculty: true,
+            academicDepartment: true
+        }
+    });
+    return result;
+};
+
+const updateFaculty = async (id: string, payload: Partial<Faculty>): Promise<Faculty> => {
+    const result = await prisma.faculty.update({
+        where: {
+            id
+        },
+        data: payload,
+        include: {
+            academicFaculty: true,
+            academicDepartment: true
+        }
+    });
+    return result;
+};
+
+const deleteFaculty = async (id: string): Promise<Faculty> => {
+    const result = await prisma.faculty.delete({
+        where: {
+            id
+        },
+        include: {
+            academicFaculty: true,
+            academicDepartment: true
+        }
+    });
+    return result;
+};
+
+const assignCourses = async (
+    id: string,
+    payload: string[]
+): Promise<CourseFaculty[]> => {
+    await prisma.courseFaculty.createMany({
+        data: payload.map((courseId) => ({
+            facultyId: id,
+            courseId: courseId
+        }))
+    })
+
+    const assignCoursesData = await prisma.courseFaculty.findMany({
+        where: {
+            facultyId: id
+        },
+        include: {
+            course: true
+        }
+    })
+
+    return assignCoursesData;
+}
+
+const removeCourses = async (
+    id: string,
+    payload: string[]
+): Promise<CourseFaculty[] | null> => {
+    await prisma.courseFaculty.deleteMany({
+        where: {
+            facultyId: id,
+            courseId: {
+                in: payload
+            }
+        }
+    })
+
+    const assignCoursesData = await prisma.courseFaculty.findMany({
+        where: {
+            facultyId: id
+        },
+        include: {
+            course: true
+        }
+    })
+
+    return assignCoursesData
+}
+
+
+export const FacultyService = {
+    createFaculty,
+    getAllFaculty,
+    getFaculty,
+    updateFaculty,
+    deleteFaculty,
+    assignCourses,
+    removeCourses
+};
