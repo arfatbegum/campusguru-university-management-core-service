7d288b28f48a87ee8808ab9b9468e2ccb53d877f
arfat132 <arfatbegum90@gmail.com>
2023-09-20 01:53:24 +0300

    [Done] get student courses, CourseSchedules, AcademicInfo

--- src/app/modules/student/student.routes.ts
+++ src/app/modules/student/student.routes.ts
@@ -7,27 +7,44 @@ import { StudentValidation } from './student.validations';
 
 const router = express.Router();
 
-router.get('/',
-    StudentController.getAllStudent);
+router.get('/', StudentController.getAllStudent);
 
 router.get('/:id', StudentController.getStudent);
+router.get(
+  '/my-courses',
+  auth(ENUM_USER_ROLE.STUDENT),
+  StudentController.myCourses
+);
+
+router.get(
+  '/my-course-schedules',
+  auth(ENUM_USER_ROLE.STUDENT),
+  StudentController.getMyCourseSchedules
+);
+router.get(
+  '/my-academic-info',
+  auth(ENUM_USER_ROLE.STUDENT),
+  StudentController.myAcademicInfo
+);
 
 router.post(
-    '/',
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    validateRequest(StudentValidation.create),
-    StudentController.createStudent
+  '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(StudentValidation.create),
+  StudentController.createStudent
 );
 
 router.patch(
-    '/:id',
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    validateRequest(StudentValidation.update),
-    StudentController.updateStudent)
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(StudentValidation.update),
+  StudentController.updateStudent
+);
 
 router.delete(
-    '/:id',
-    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-    StudentController.deleteStudent);
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  StudentController.deleteStudent
+);
 
 export const StudentRoutes = router;
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
     Prisma,
-    Student
+    Student,
+    StudentEnrolledCourseStatus
 } from '@prisma/client';
 import { paginationHelpers } from '../../../helpers/paginationHelper';
 import { IGenericResponse } from '../../../interfaces/common';
@@ -9,6 +10,7 @@ import { IPaginationOptions } from '../../../interfaces/pagination';
 import prisma from '../../../shared/prisma';
 import { studentRelationalFields, studentRelationalFieldsMapper, studentSearchableFields } from './student.constants';
 import { IStudentFilterRequest } from './student.interface';
+import { StudentUtils } from './student.utils';
 
 const createStudent = async (data: Student): Promise<Student> => {
     const result = await prisma.student.create({
@@ -138,11 +140,138 @@ const deleteStudent = async (id: string): Promise<Student> => {
     return result;
 }
 
+const myCourses = async (
+    authUserId: string,
+    filter: {
+        courseId?: string | undefined,
+        academicSemesterId?: string | undefined
+    }
+) => {
+    if (!filter.academicSemesterId) {
+        const currentSemester = await prisma.academicSemester.findFirst({
+            where: {
+                isCurrent: true
+            }
+        });
+        filter.academicSemesterId = currentSemester?.id;
+    }
+
+    const result = await prisma.studentEnrolledCourse.findMany({
+        where: {
+            student: {
+                studentId: authUserId
+            },
+            ...filter
+        },
+        include: {
+            course: true
+        }
+    });
+
+    return result;
+};
+
+const getMyCourseSchedules = async (
+    authUserId: string,
+    filter: {
+        courseId?: string | undefined,
+        academicSemesterId?: string | undefined
+    }) => {
+    if (!filter.academicSemesterId) {
+        const currentSemester = await prisma.academicSemester.findFirst({
+            where: {
+                isCurrent: true
+            }
+        });
+        filter.academicSemesterId = currentSemester?.id;
+    }
+
+    const studentEnrolledCourses = await myCourses(authUserId, filter);
+    const studentEnrolledCourseIds = studentEnrolledCourses.map((item) => item.courseId);
+    const result = await prisma.studentSemesterRegistrationCourse.findMany({
+        where: {
+            student: {
+                studentId: authUserId
+            },
+            semesterRegistration: {
+                academicSemester: {
+                    id: filter.academicSemesterId
+                }
+            },
+            offeredCourse: {
+                course: {
+                    id: {
+                        in: studentEnrolledCourseIds
+                    }
+                }
+            }
+        },
+        include: {
+            offeredCourse: {
+                include: {
+                    course: true
+                }
+            },
+            offeredCourseSection: {
+                include: {
+                    offeredCourseClassSchedules: {
+                        include: {
+                            room: {
+                                include: {
+                                    building: true
+                                }
+                            },
+                            faculty: true
+                        }
+
+                    }
+                }
+            }
+        }
+    });
+    return result;
+};
+
+const getMyAcademicInfo = async (authUserId: string): Promise<any> => {
+    const academicInfo = await prisma.studentAcademicInfo.findFirst({
+        where: {
+            student: {
+                studentId: authUserId
+            }
+        }
+    });
+
+    const enrolledCourses = await prisma.studentEnrolledCourse.findMany({
+        where: {
+            student: {
+                studentId: authUserId
+            },
+            status: StudentEnrolledCourseStatus.COMPLETED
+        },
+        include: {
+            course: true,
+            academicSemester: true
+        },
+        orderBy: {
+            createdAt: 'asc'
+        }
+    });
+
+    const groupByAcademicSemesterData = StudentUtils.groupByAcademicSemester(enrolledCourses);
+
+    return {
+        academicInfo,
+        courses: groupByAcademicSemesterData
+    }
+}
 
 export const StudentService = {
     createStudent,
     getAllStudent,
     getStudent,
     updateStudent,
-    deleteStudent
+    deleteStudent,
+    myCourses,
+    getMyCourseSchedules,
+    getMyAcademicInfo
 };
